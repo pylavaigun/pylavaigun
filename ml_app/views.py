9ce703a57c041a7a6d3c0d6e8f41456a683fc3c6
Di Wrk <digi30571@gmail.com>
2023-09-19 12:05:28 +0300

    add auth app

--- ml_app/views.py
+++ ml_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+# Главная страница
+def index(request):
+    template = 'index.html'
+    return render(request, template)
+
 # Create your views here.
--- pylavaigun/settings.py
+++ pylavaigun/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '185.60.134.195',
+    '127.0.0.1',
 ]
 
 
@@ -56,10 +57,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pylavaigun.urls'
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'ml_app/templates'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
