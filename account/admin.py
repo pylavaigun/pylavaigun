41f72aca04a1f18b7da4e8d9a79ec5d585e2ce20
Di Wrk <digi30571@gmail.com>
2023-10-02 17:10:49 +0300

    adding account

--- account/admin.py
+++ account/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from account.models import UserManager, User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'username', 'password',]
+
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
