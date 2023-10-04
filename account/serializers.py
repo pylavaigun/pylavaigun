e4e63dc4c56759cfe0074f0d28bd4314c88c038b
Di Wrk <digi30571@gmail.com>
2023-10-04 17:07:47 +0300

    adding jwt

--- account/serializers.py
+++ account/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import User
 
-class RegistrationSerializer(serializers.ModelSerializer):
-    """ Сериализация регистрации пользователя и создания нового. """
+"""class RegistrationSerializer(serializers.ModelSerializer):
+
 
     password = serializers.CharField(
         max_length=128,
@@ -17,4 +17,28 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ['email', 'username', 'password', 'token']
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        return User.objects.create_user(**validated_data)"""
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model # If used custom user model
+
+UserModel = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+
+        user = UserModel.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+
+        return user
+
+    class Meta:
+        model = UserModel
+        # Tuple of serialized model fields (see link [2])
+        fields = ( "email", "username", "password", )
