d8f454058151f6e3a62071ef24aeabb337c5d239
Di Wrk <digi30571@gmail.com>
2023-10-05 14:48:35 +0300

    adding login

--- account/serializers.py
+++ account/serializers.py
@@ -38,11 +38,11 @@ class LoginSerializer(serializers.Serializer):
         # LoginSerializer значение valid. В случае входа пользователя в систему
         # это означает подтверждение того, что присутствуют адрес электронной
         # почты и то, что эта комбинация соответствует одному из пользователей.
-        email = data.get('email', None)
+        username = data.get('username', None)
         password = data.get('password', None)
 
         # Вызвать исключение, если не предоставлена почта.
-        if email is None:
+        if username is None:
             raise serializers.ValidationError(
                 'An email address is required to log in.'
             )
@@ -53,29 +53,18 @@ class LoginSerializer(serializers.Serializer):
                 'A password is required to log in.'
             )
 
-        # Метод authenticate предоставляется Django и выполняет проверку, что
-        # предоставленные почта и пароль соответствуют какому-то пользователю в
-        # нашей базе данных. Мы передаем email как username, так как в модели
-        # пользователя USERNAME_FIELD = email.
-        user = authenticate(username=email, password=password)
+        user = authenticate(username=username, password=password)
 
-        # Если пользователь с данными почтой/паролем не найден, то authenticate
-        # вернет None. Возбудить исключение в таком случае.
         if user is None:
             raise serializers.ValidationError(
                 'A user with this email and password was not found.'
             )
 
-        # Django предоставляет флаг is_active для модели User. Его цель
-        # сообщить, был ли пользователь деактивирован или заблокирован.
-        # Проверить стоит, вызвать исключение в случае True.
         if not user.is_active:
             raise serializers.ValidationError(
                 'This user has been deactivated.'
             )
 
-        # Метод validate должен возвращать словать проверенных данных. Это
-        # данные, которые передются в т.ч. в методы create и update.
         return {
             'email': user.email,
             'username': user.username,
