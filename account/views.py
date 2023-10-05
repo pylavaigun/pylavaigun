e36667a006233c47eee8162cb4103c63fb0bdf0d
Di Wrk <digi30571@gmail.com>
2023-10-05 15:41:29 +0300

    adding login

--- account/views.py
+++ account/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer, LoginSerializer, LoginSerializers
+from .serializers import RegistrationSerializer, LoginSerializer
 
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
